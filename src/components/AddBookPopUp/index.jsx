f3c88cb952d8ca3a8d9ac4adcddba23ef94ea59f
jnip2 <jnip2@my.bcit.ca>
2023-11-14 22:06:01 -0800

    add close handlers and bg

--- src/components/AddBookPopUp/index.jsx
+++ src/components/AddBookPopUp/index.jsx
@@ -1,34 +1,41 @@
 import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
 
-export default function AddBookPopUp() {
+export default function AddBookPopUp({ onClose }) {
+
     return (
         <>
-            <div style={styles.container}>
-                <div style={{ display: 'flex' }}>
-                    <form style={styles.formContainer}>
-                        <div style={styles.inputContainer}>
-                            <label style={styles.labelContainer}>
-                                Title
-                                <input style={styles.inputBox} type='text' />
-                            </label>
-                            <label style={styles.labelContainer}>
-                                Author
-                                <input style={styles.inputBox} type='text' />
-                            </label>
-                            <label style={styles.labelContainer}>
-                                Genre
-                                <input style={styles.inputBox} type='text' />
-                            </label>
+            <div style={styles.popUpBg} onClick={onClose}>
+                <div style={styles.popUpContainer}>
+                    <div style={styles.container}>
+                        <div style={{ display: 'flex' }}>
+                            <form style={styles.formContainer}>
+                                <div style={styles.inputContainer}>
+                                    <label style={styles.labelContainer}>
+                                        Title
+                                        <input style={styles.inputBox} type='text' />
+                                    </label>
+                                    <label style={styles.labelContainer}>
+                                        Author
+                                        <input style={styles.inputBox} type='text' />
+                                    </label>
+                                    <label style={styles.labelContainer}>
+                                        Genre
+                                        <input style={styles.inputBox} type='text' />
+                                    </label>
+                                </div>
+                                <div style={styles.buttonContainer}>
+                                    <button type='submit' style={{ width: 285, height: 50 }}>Add to Library</button>
+                                </div>
+                            </form>
                         </div>
-                        <div style={styles.buttonContainer}>
-                            <button type='submit' style={{ width: 285, height: 50 }}>Add to Library</button>
+                        <div style={styles.iconContainer} onClick={onClose}>
+                            <CloseIcon />
                         </div>
-                    </form>
-                </div>
-                <div style={styles.iconContainer}>
-                    <CloseIcon />
+                    </div>
                 </div>
             </div>
+
         </>
     )
 }
@@ -38,7 +45,8 @@ const styles = {
         width: 734,
         height: 484,
         border: '1px solid black',
-        position: 'relative'
+        position: 'relative',
+        background: 'white'
     },
     formContainer: {
         display: 'flex',
@@ -70,5 +78,20 @@ const styles = {
         justifyContent: 'center',
         width: 734,
         marginLeft: -26
+    },
+    popUpBg: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0, 0, 0, 0.7)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    popUpContainer: {
+        margin: 'auto', // Center horizontally and vertically
+        position: 'relative',
     }
 }
