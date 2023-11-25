1d028fd6f291e76074d14cf9deed3f34086c9929
Navid Memari <work.nmemari@gmail.com>
2023-11-24 18:45:32 -0800

    added create and delete funtionality

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -18,26 +18,8 @@ export default function Home() {
         setIsAddBookPopUpOpen(false)
     }
     return (
-        <>
-            <div className="lib__container">
-                <div className="lib__sort">
-                    <h3 className="lib__sort_title">Library</h3>
-                    <label>
-                        Sort By:
-                        <select>
-                            <option>All</option>
-                            <option>Recent</option>
-                            <option>Shuffle</option>
-                        </select>
-                    </label>
-                </div>
-                <button
-                    className="lib__btn"
-                    onClick={handleOpen}
-                >Add</button>
-            </div>
+        <div>
             <BookList />
-            {isAddBookPopUpOpen && <AddBookPopUp onClose={handleClose} />}
-        </>
+        </div>
     )
 }
